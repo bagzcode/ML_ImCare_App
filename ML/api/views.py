8904ee5a80d3d3534428c8ed0e6eb39c6a9b95ec
bagzcode <bagus.manuaba@gmail.com>
2019-06-18 16:07:32 +0700

    update 1

--- ML/api/views.py
+++ ML/api/views.py
@@ -45,10 +45,12 @@ class Train(views.APIView):
             dt_source = pd.read_csv("./models/data_mayapada.csv")#target to mayapada data
             feature_names = list(dt_source)[0:dt_source.shape[1]-2]
             target_names = ["Normal","Level 1","Level 2","Level 3"]
+            target_data = dt_source['CLASS'].values
         elif data_source == "new_mayapada":
             dt_source = pd.read_csv("./models/mayapada.csv")#target to new_mayapada data
             feature_names = list(dt_source)[0:dt_source.shape[1]-1]
             target_names = ["sehat","tidak sehat"]
+            target_data = dt_source['ket'].values
         
         mapping = dict(zip(np.unique(target_data), target_names))
         x_train = pd.DataFrame(dt_source, columns=feature_names)
